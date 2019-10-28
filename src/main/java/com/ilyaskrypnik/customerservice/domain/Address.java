f7f9e10edfee0cfdeab82411e157f4650da2779f
Ilya <ilyaskrypnik@gmail.com>
2019-10-28 16:57:35 +0700

    Change address and customer entities

--- src/main/java/com/ilyaskrypnik/customerservice/domain/Address.java
+++ src/main/java/com/ilyaskrypnik/customerservice/domain/Address.java
@@ -1,24 +1,31 @@
 package com.ilyaskrypnik.customerservice.domain;
 
 import lombok.Data;
-import lombok.NonNull;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "address")
 @Data
 public class Address {
-    @NonNull
+
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Size(max = 255)
     private String country;
+    @Size(max = 255)
     private String region;
+    @Size(max = 255)
     private String city;
+    @Size(max = 255)
     private String street;
+    @Size(max = 255)
     private String house;
+    @Size(max = 255)
     private String flat;
     private LocalDateTime created;
     private LocalDateTime modified;
--- src/main/java/com/ilyaskrypnik/customerservice/domain/Customer.java
+++ src/main/java/com/ilyaskrypnik/customerservice/domain/Customer.java
@@ -1,23 +1,41 @@
 package com.ilyaskrypnik.customerservice.domain;
 
 import lombok.Data;
-import lombok.NonNull;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "customer")
 @Data
 public class Customer {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @NonNull
-    private Long registeredAddressId;
-    @NonNull
-    private Long actualAddressId;
+
+    @NotNull
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "registered_address_id", nullable = false)
+    private Address registeredAddress;
+
+    @NotNull
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "actual_address_id", nullable = false)
+    private Address actualAddress;
+
+    @Column(name = "first_name")
+    @Size(max = 255)
     private String firstName;
+    @Column(name = "last_name")
+    @Size(max = 255)
     private String lastName;
+    @Column(name = "middle_name")
+    @Size(max = 255)
     private String middleName;
-    @NonNull
+    @NotNull
+    @Size(max = 6)
     private String sex;
 }
